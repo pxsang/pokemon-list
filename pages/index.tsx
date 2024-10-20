19a3482dd612d41e4d02e623ee612c5fdf20a0cc
Cris Sang <pxsang194@gmail.com>
2024-10-20 15:51:08 +0700

    Mock UI Pagination component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import PokemonList from "./components/PokemonList/PokemonList";
 import PokemonTypes from "./components/PokemonTypes/PokemonTypes";
+import Pagination from "./components/Pagination/Pagination";
 
 export default function Home() {
   return (
     <div>
       <PokemonTypes />
       <PokemonList />
-      <div>Pagination</div>
+      <Pagination />
     </div>
   );
 }
